ea14c733c489f36f85e69015617a832789e9fa7d
JDuke3 <duke10@purdue.edu>
2026-09-11 14:58:33 -0400

    mobile fix 3

--- book-club/app/Info/page.tsx
+++ book-club/app/Info/page.tsx
@@ -6,14 +6,14 @@ export default function Info() {
 
     <main className="min-h-screen bg-slate-900 text-white overflow-hidden flex flex-col items-center justify-center pt-24 px-10">
       <Navbar />
-        <div className="flex flex-col rounded-2xl items-center bg-slate-950">
+        <div className="flex flex-col rounded-2xl items-center bg-slate-950 max-w-3xl">
           
         <h1 className=" p-10 text-4xl font-bold tracking-tight  text-zinc-900 dark:text-zinc-100">
         Info:
         </h1>
 
         <div className="p-10">
-        <ul className="flex p-20 flex-col items-left w-full max-w-7xl list-disc space-y-5 bg-slate-800 rounded-2xl text-lg text-left">
+        <ul className="flex p-10 flex-col items-left w-full max-w-8xl list-disc space-y-5 bg-slate-800 rounded-2xl text-lg text-left">
             <li>Book Club is a reading group with rotating book selections and semi-monthly discussion meetings.</li>
             <li>I will try to update the website monthly with a pair
                 of books to choose from (one shorter and one longer).</li>
